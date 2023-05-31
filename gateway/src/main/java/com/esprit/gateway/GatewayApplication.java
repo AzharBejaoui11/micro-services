36c079215f2559d6f1030551d96b75be8b8f15b7
= <=>
2023-05-31 01:19:06 +0100

    gatewayApi

--- gateway/src/main/java/com/esprit/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/esprit/gateway/GatewayApplication.java
@@ -2,6 +2,8 @@ package com.esprit.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
 public class GatewayApplication {
